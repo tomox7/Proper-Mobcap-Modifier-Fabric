4dd49ce92bdf6a2be4d95487c83faaa025419a43
tabaz <tabaz@example.com>
2024-12-24 11:15:16 +0900

    aa

---
+++ src/client/java/jp/priv/util/ProperMobcapModifierFabricClient.java
@@ -0,0 +1,10 @@
+package jp.priv.util;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProperMobcapModifierFabricClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
